b51aabcc03f446bf9de1ee764d2bc049d7058bff
Sigve Sjømæling Nordgaard <sigvesn@dhcp-10-22-155-62.wlan.ntnu.no>
2016-11-03 14:09:42 +0100

    Update arbitrator.py

--- arbitrator.py
+++ arbitrator.py
@@ -1,6 +1,7 @@
+from bbcon import BBCON as bb
 class Arbitrator():
-	def __init__(self, bbcon = None):
-		self.bbcon = bbcon
+	def __init__(self):
+		#self.bbcon = bb()
 
 	def choose_action(self, stochastic = True):
 		#Chooses a winning behavior from bbqon stochastically(default) or deterministically

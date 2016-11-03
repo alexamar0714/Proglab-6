dd5b512b54114242dc2b66298b1e9e4f7a4d16a5
Sigve Sjømæling Nordgaard <sigvesn@dhcp-10-22-155-62.wlan.ntnu.no>
2016-11-03 10:31:51 +0100

    Demo for arbitrator

---
+++ arbitrator.py
@@ -0,0 +1,21 @@
+class Arbitrator():
+	def __init__(self, bbcon = None):
+		self.bbcon = bbcon
+
+	def choose_action(self, stochastic = True):
+		#Chooses a winning behavior from bbqon stochastically(default) or deterministically
+		#returns the behaviors motor recomandations and halt request
+		behaviors = self.bbcon.get_behaviors()
+		
+		if not stochastic:													
+			winning_behavior = max(behaviors, key=lambda x: x.get_weight())
+		else:																	 
+			i = 0
+			_sum = sum(map(lambda x: x.get_weight(), behaviors))
+			rand = random.uniform(0,_sum)
+			for behavior in behaviors:
+				i += behavior.get_weight()
+				if rand < i:
+					winning_behavior = behavior
+
+		return behavior.get_motor_recomandation(), behavior.get_halt_request()

8ef909087619e5c2dc6f7684385f699bcd96a2c9
Dodib <duong_mnb@hotmail.com>
2016-11-08 13:31:44 +0100

    Update avoidobj.py

--- avoidobj.py
+++ avoidobj.py
@@ -9,7 +9,7 @@ class AvoidObj():
     match_degree = 0
     motor_recc = None
 
-    def _init_(self, bbcon = None, ultra = None, ir_prox = None):
+    def _init_(self, bbcon, ultra, ir_prox):
         self.ultra = ultra
         self.ir_prox = ir_prox
         self.priority = 8

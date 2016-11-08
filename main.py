380e14d6b39b74437e5799160a5fe36a4be20c01
Dodib <duong_mnb@hotmail.com>
2016-11-08 13:06:06 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from ultrasonic import Ultrasonic
+from irproximity_sensor import IRProximitySensor as IR
+from camera import Camera
+from motors import Motors
+from reflectance_sensors import ReflectanceSensors
+from avoidobj import AvoidObj
+from behavior_line_follower import Behavior_line_follower
+from behaviour_avoid_blue import  Behaviour_avoid_blue
+from arbitrator import Arbitrator
+from fuck_you_behaviour import Fuck_you_behaviour as fub
+from zumo_button import ZumoButton
+
+
+def start():
+    arb = Arbitrator()
+    bbcon = BBCON(arb)
+    reflect_sens = ReflectanceSensors(True)
+    cam = Camera()
+    motor = Motors()
+    ir = IR()
+    ultra = Ultrasonic()
+    bbcon.add_behaviour(AvoidObj(bbcon=bbcon, ultra=ultra, ir_prox=ir))
+    bbcon.add_behaviour(Behavior_line_follower())
+    bbcon.add_behaviour(Behaviour_avoid_blue(bb=bbcon, cam=cam, ultra=ultra))
+    bbcon.add_behaviour(fub(bb=bbcon))
+    bbcon.add_sensob(reflect_sens)
+    bbcon.add_sensob(cam)
+    bbcon.add_sensob(ir)
+    bbcon.add_sensob(ultra)
+    bbcon.add_motobs(motor)
+
+    butt = ZumoButton()
+    butt.wait_for_press()
+
+    while True:
+        bbcon.run_one_timestep()

e91b9a5892e7f882b9196a570d4c888744da1124
Sigve Sjømæling Nordgaard <sigvesn@dhcp-10-22-41-28.wlan.ntnu.no>
2016-11-10 14:23:19 +0100

    up

--- behavior_line_follower.py
+++ behavior_line_follower.py
@@ -8,7 +8,7 @@ class Behaviour_line_follower():
 		self.motor_recommandations = [('f',0,0)]
 		self.active_flag = True
 		self.halt_request = False
-		self.priority = 1
+		self.priority = 10
 		self.match_degree = 0.0
 		self.weight = self.priority*self.match_degree
 

4a9803728c0c1d9c53a087b6d4b854af1e1e97d3
Dodib <duong_mnb@hotmail.com>
2016-11-10 13:27:19 +0100

    Update bbcon.py

--- bbcon.py
+++ bbcon.py
@@ -40,6 +40,7 @@ class BBCON():
         self.update_all_behaviours()
         motor_recc, halt_req = self.arbitrator.choose_action(stochastic = True)
         self.update_motobs(motor_recc, halt_req)
+        print(self.active_behaviours)
         self.reset_all_sensobs()
 
     def update_all_sensobs(self):

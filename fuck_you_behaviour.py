4cafa814271737fb6b7b419a4833b41b86cdf177
Dodib <duong_mnb@hotmail.com>
2016-11-10 13:29:10 +0100

    Update fuck_you_behaviour.py

--- fuck_you_behaviour.py
+++ fuck_you_behaviour.py
@@ -5,7 +5,7 @@ class Fuck_you_behaviour():
         self.motor_recommandations = [('f', 0.5, 0.5)]
         self.active_flag = True
         self.halt_request = False
-        self.priority = 2
+        self.priority = 0.1
         self.match_degree = 1
         self.weight = self.priority*self.match_degree
 

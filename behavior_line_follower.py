9d5c7cb65c90627011b82c3a50a8a6858edeebc1
Dodib <duong_mnb@hotmail.com>
2016-11-11 12:20:02 +0100

    Update behavior_line_follower.py

--- behavior_line_follower.py
+++ behavior_line_follower.py
@@ -56,7 +56,7 @@ class Behaviour_line_follower():
 
 			total = l - r
 			print('Total:',total)		
-			speed = 0.1*total
+			speed = 0.3*total
 			self.match_degree =1000
 
 			if total < 1 and total > -1:

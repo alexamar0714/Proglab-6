c8ecd18ff9c06d95418d610bcd7f36a77b105192
Sigve Sjømæling Nordgaard <sigvesn@dhcp-10-22-41-28.wlan.ntnu.no>
2016-11-11 14:04:57 +0100

    Update

--- fuck_you_behaviour.py
+++ fuck_you_behaviour.py
@@ -2,7 +2,7 @@ class Fuck_you_behaviour():
 	
     def __init__(self, bb=None):
         self.bbcon = bb
-        self.motor_recommandations = [('f', 0.20, 0.2)]
+        self.motor_recommandations = [('f', 0.20, 0)]
         self.active_flag = True
         self.halt_request = False
         self.priority = 0.1

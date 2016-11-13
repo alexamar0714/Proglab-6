3c403c582e453f3d6f7e13a8f57fb3072b3dadd8
Dodib <duong_mnb@hotmail.com>
2016-11-13 17:46:16 +0100

    Update fuck_you_behaviour.py

--- fuck_you_behaviour.py
+++ fuck_you_behaviour.py
@@ -1,25 +1,27 @@
 class Fuck_you_behaviour():
 	
-    def __init__(self, bb=None):
-        self.bbcon = bb
-        self.motor_recommandations = [('f', 0.20, 0)]
-        self.active_flag = True
-        self.halt_request = False
-        self.priority = 0.1
-        self.match_degree = 1
-        self.weight = self.priority*self.match_degree
+	#this behaviour is just so that when no other behaviour is active, ie all others have weight 0
+	#this will make it move forward.
+    	def __init__(self, bb=None):
+        	self.bbcon = bb
+        	self.motor_recommandations = [('f', 0.20, 0)]
+        	self.active_flag = True
+        	self.halt_request = False
+        	self.priority = 0.1
+        	self.match_degree = 1
+        	self.weight = self.priority*self.match_degree
 
-    def get_weight(self):
-        return self.weight
+    	def get_weight(self):
+        	return self.weight
 
-    def get_motor_recc(self):
-        return self.motor_recommandations
+    	def get_motor_recc(self):
+        	return self.motor_recommandations
 	
-    def get_halt_request(self):
-        return self.halt_request
+    	def get_halt_request(self):
+        	return self.halt_request
 
-    def update(self):
-        pass
+    	def update(self):
+        	pass
 
 
         

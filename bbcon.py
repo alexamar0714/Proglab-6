8b0f753cdfb77e3e5764b4d2105a1deab0ac2ff5
Dodib <duong_mnb@hotmail.com>
2016-11-13 17:58:35 +0100

    Update bbcon.py

--- bbcon.py
+++ bbcon.py
@@ -36,13 +36,16 @@ class BBCON():
     def deactivate_behaviour(self, behaviour):
         if behaviour in self.active_behaviours:
             self.active_behaviours.remove(behaviour)
-
+    
+    #this method is mainly for class AvoidObj, to tell bbcon that ultrasonic has detected something
     def ultra_detected(self, booly):
         self.ultra_detect = booly
         
     def run_one_timestep(self):
         self.update_all_sensobs()
         self.update_all_behaviours()
+        #since ultra has detected something, start code to activate camera and avoid_blue behaviour
+        #this is done like this because the camera and avoid_blue_behaviour takes a lot of resources(time)
         if self.ultra_detect:
             print("camera is on")
             last_sens = len(self.sensobs) - 1
@@ -61,6 +64,7 @@ class BBCON():
         
 
     def update_all_sensobs(self):
+        #update all sensobs except for the last one, if you see in main.py, the last obj, is camera
         for ind, sensob in enumerate(self.sensobs):
             if ind == len(self.sensobs) - 1:
                 continue
@@ -68,6 +72,7 @@ class BBCON():
                 sensob.update()
                 
     def update_all_behaviours(self):
+        #update all behaviours except for the last one, if you see in main.py, the last obj is avoid_blue
         for ind, behave in enumerate(self.behaviours):
             if ind == len(self.behaviours) - 1:
                 continue
@@ -79,41 +84,32 @@ class BBCON():
             sensors.reset()
 
     def update_motobs(self, motor_recc, halt_req):
+        #motor_recc is a list of tuples with 3 variables = ("direction", speed, duration)
         for tuples in motor_recc:
             if halt_req or motor_recc[0] == None:
                 for motobs in self.motobs:
-                    print("STOPPING")
+                    #print("STOPPING")
                     motobs.stop()
             elif tuples[0] == "f":
-                print('f-drive:', motor_recc)
+                #print('f-drive:', motor_recc)
                 for motobs in self.motobs:
                     motobs.stop()
                     motobs.forward(speed = tuples[1], dur = tuples[2])
             elif tuples[0] == "b":
-                print('b-drive:', motor_recc)
+                #print('b-drive:', motor_recc)
                 for motobs in self.motobs:
                     motobs.stop()
                     motobs.backward(speed = tuples[1], dur = tuples[2])
             elif tuples[0] == "r":
-                print('r-drive:', motor_recc)
+                #print('r-drive:', motor_recc)
                 for motobs in self.motobs:
                     motobs.stop()
                     motobs.right(speed = tuples[1], dur = tuples[2])
             elif tuples[0] == "l":
-                print('l-drive:', motor_recc)
+                #print('l-drive:', motor_recc)
                 for motobs in self.motobs:
                     motobs.stop()
                     motobs.left(speed = tuples[1], dur = tuples[2])
-            elif tuples[0] == "base":
-                for motobs in self.motobs:
-                    motobs.base(speed = tuples[1], dur = tuples[2])
-            elif tuples[0] == "inc_r":
-                for motobs in self.motobs:
-                    motobs.inc_r(speed = tuples[1], dur = tuples[2])
-            elif tuples[0] == "inc_l":
-                for motobs in self.motobs:
-                    motobs.inc_l(speed = tuples[1], dur = tuples[2])
-            print("sleepend")
             time.sleep(tuples[2]/10)
             
 
